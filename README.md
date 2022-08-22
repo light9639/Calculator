4d26d5a9d56501b69cb6269f104f44ef4c0c74d0
light9639 <dong963939@gmail.com>
2022-08-22 22:05:19 +0900

    2차 수정

---
+++ README.md
@@ -0,0 +1,27 @@
+# :zap: Calculator 프로젝트 소개
+:octocat: 바로가기 https://light9639.github.io/Calculator/
+
+![화면 캡처 2022-08-22](https://raw.githubusercontent.com/light9639/PortFolio/main/assets/light9639.github.io_PortFolio_.png)
+
+:sparkles: React 계산기 사이트입니다. :sparkles:
+
+## :rocket: 시작하는 법
+미리보기를 원하신다면 다운로드 후 yarn을 설치하고
+```bash
+yarn vite
+# or
+yarn dev
+```
+를 누르고 http://127.0.0.1:5173/ 로 접속하면 미리보기 화면을 띄울 수 있습니다.
+## :calendar: 개발인원 및 기간
+- 1인 개발 2022.08.20 ~ 2022.08.22(3일 소요)
+## :dart: 개발 목적
+-
+## :black_nib: 구현기능
+-
+## :hammer_and_wrench: 적용 기술
+### :zap: React
+-
+
+### :zap: Vite
+- WebPack을 사용하는 대신 Vite라는 2세대 번들링 툴을 사용하여 서버에서 작동하는 JS의 크기를 줄이고 속도를 높였습니다.
---
+++ src/App.jsx
@@ -0,0 +1,195 @@
+import './App.css';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+function App() {
+  const [calc, setCalc] = useState("");
+  const [operCheck, setOperCheck] = useState(true);
+  const [pointCheck, setPointCheck] = useState(true);
+
+  const getNum = (e) => {
+    setCalc((prev) => prev + e.target.value);
+    setOperCheck(true);
+  };
+
+  const getOper = (e) => {
+    if (operCheck) {
+      setCalc((prev) => prev + e.target.value);
+      setOperCheck(false);
+    }
+  };
+
+  const getPoint = (e) => {
+    if (calc.length === 0) {
+      return;
+    }
+    if (pointCheck) {
+      setCalc((prev) => prev + e.target.value);
+      setPointCheck(false);
+    }
+  };
+
+  const getResult = () => {
+    let replace_str = calc.replace(/×/gi, "*").replace(/÷/gi, "/");
+
+    if (isNaN(eval(replace_str))) {
+      setCalc("");
+    } else if (eval(replace_str) == Infinity) {
+      alert("0으로 나눌수 없습니다.");
+      setCalc("");
+      return false;
+    } else {
+      setCalc((prev) => eval(replace_str));
+    }
+  };
+
+  const delCalc = () => {
+    setPointCheck(true);
+    setOperCheck(true);
+    let str = String(calc).slice(0, -1);
+    setCalc((prev) => str);
+  };
+
+  const allClear = () => {
+    setPointCheck(true);
+    setCalc((prev) => "");
+  };
+
+  return (
+    <div className="App">
+      <MainContainer>
+        <h1>React 계산기</h1>
+        <InputBar readOnly value={calc} />
+        <ButtonContainer>
+          <Button onClick={allClear}>AC</Button>
+          <Button onClick={delCalc}>DEL</Button>
+          <CalButton value="%" onClick={getOper}>
+            %
+          </CalButton>
+          <CalButton value="÷" onClick={getOper}>
+            ÷
+          </CalButton>
+          <Button value={7} onClick={getNum}>
+            7
+          </Button>
+          <Button value={8} onClick={getNum}>
+            8
+          </Button>
+          <Button value={9} onClick={getNum}>
+            9
+          </Button>
+          <CalButton value="×" onClick={getOper}>
+            ×
+          </CalButton>
+          <Button value={4} onClick={getNum}>
+            4
+          </Button>
+          <Button value={5} onClick={getNum}>
+            5
+          </Button>
+          <Button value={6} onClick={getNum}>
+            6
+          </Button>
+          <CalButton value="-" onClick={getOper}>
+            -
+          </CalButton>
+          <Button value={1} onClick={getNum}>
+            1
+          </Button>
+          <Button value={2} onClick={getNum}>
+            2
+          </Button>
+          <Button value={3} onClick={getNum}>
+            3
+          </Button>
+          <CalButton value="+" onClick={getOper}>
+            +
+          </CalButton>
+          <ZeroButton value={0} onClick={getNum}>
+            0
+          </ZeroButton>
+          <Button value="." onClick={getPoint}>
+            .
+          </Button>
+          <CalButton onClick={getResult}>=</CalButton>
+        </ButtonContainer>
+      </MainContainer>
+    </div>
+  );
+}
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const ButtonContainer = styled.div`
+  display: grid;
+  width: 40%;
+  max-width: 450px;
+  height: 50%;
+  grid-template-columns: repeat(4, 1fr);
+  grid-column-gap: 10px;
+  grid-row-gap: 10px;
+  @media screen and (max-width: 1024px) {
+    max-width: 500px;
+    width: 100%;
+  }
+  @media screen and (max-width: 640px) {
+    max-width: 98%;
+    width: 100%;
+  }
+`;
+
+const Button = styled.button`
+  background-color: #f2f3f5;
+  border: none;
+  color: black;
+  font-size: 1.5rem;
+  border-radius: 35px;
+  cursor: pointer;
+  box-shadow: 3px 3px 3px lightgray;
+  &:active {
+    margin-left: 2px;
+    margin-top: 2px;
+    box-shadow: none;
+  }
+`;
+
+const CalButton = styled(Button)`
+  font-size: 2rem;
+  color: white;
+  background-color: #4b89dc;
+`;
+
+const ZeroButton = styled(Button)`
+  grid-column: 1/3;
+`;
+
+const InputBar = styled.input`
+  width: 40%;
+  max-width: 425px;
+  height: 65px;
+  margin-bottom: 10px;
+  border-radius: 10px;
+  font-size: 30px;
+  border: 2px solid #4b89dc;
+  text-align: right;
+  padding-right: 20px;
+  &:focus {
+    outline: none;
+  }
+  @media screen and (max-width: 1024px) {
+    max-width: 460px;
+    width: 100%;
+  }
+  @media screen and (max-width: 640px) {
+    max-width: 90%;
+    width: 100%;
+  }
+`;
+
+export default App;
